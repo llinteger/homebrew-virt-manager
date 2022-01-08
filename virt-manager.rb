ecb467c8ed39f2d0ff39869deadd65b98d0f3a75
Su Yue <l@damenly.su>
2022-01-08 19:36:58 +0800

    Fix typo
    Signed-off-by: Su Yue <l@damenly.su>

--- virt-manager.rb
+++ virt-manager.rb
@@ -23,7 +23,7 @@ class VirtManager < Formula
   depends_on "python"
   depends_on "spice-gtk"
   depends_on "vte3"
-  depedns_on "pygobject3"
+  depends_on "pygobject3"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
